6d110dfdbea091291ebefad33725d874cef4d721
Dennis Lambe Jr. <malsyned@malsyned.net>
2014-10-02 23:29:08 -0400

    Support expected failures. Better (I think) output format

--- pfstest.c
+++ pfstest.c
@@ -3,37 +3,93 @@
 #include <setjmp.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#define RESULT_FAIL 1
+#define RESULT_PASS 1
+#define RESULT_FAIL 2
+#define RESULT_IGNORE 3
 
-jmp_buf test_jmp_buf;
+static jmp_buf test_jmp_buf;
+static bool fail_expected;
 
 static int passed;
 static int failed;
 static int ignored;
 
+void fail_with_printer(void (*printer)(const void *), const void *object)
+{
+    printf("FAIL");
+    printf("\n");
+    printer(object);
+    printf("\n");
+    longjmp(test_jmp_buf, RESULT_FAIL);
+}
+
+void message_printer(const void *object)
+{
+    const char *message = object;
+    printf("    %s", message);
+}
+
 void fail(const char *message)
 {
-    printf("FAIL: %s\n", message);
-    longjmp(test_jmp_buf, RESULT_FAIL);
+    fail_with_printer(message_printer, message);
+}
+
+static void pass()
+{
+    longjmp(test_jmp_buf, RESULT_PASS);
+}
+
+static void ignore()
+{
+    longjmp(test_jmp_buf, RESULT_IGNORE);
 }
 
 void run_test(struct pfstest *the_test)
 {
-    printf("%s (%s:%d) ", the_test->name, the_test->file, the_test->line);
+    printf("%s:%d: %s() ", the_test->file, the_test->line, the_test->name);
 
-    if (the_test->flags & PFSTEST_FLAG_IGNORED) {
-        printf("IGNORED\n");
-        ignored++;
-    } else if (setjmp(test_jmp_buf) == 0) {
-        the_test->function();
-        if (the_test->flags & PFSTEST_FLAG_EXPECT_FAIL) {
-            fail("Test passed, expecting failure");
-        }
-        passed++;
-        printf("PASS\n");
-    } else {
-        failed++;
+    if (the_test->flags & PFSTEST_FLAG_EXPECT_FAIL)
+        fail_expected = true;
+    else
+        fail_expected = false;
+
+    switch (setjmp(test_jmp_buf)) {
+        case 0:
+            if (the_test->flags & PFSTEST_FLAG_IGNORED) {
+                ignore();
+            }
+            the_test->function();
+            pass();
+            break;
+        case RESULT_PASS:
+            if (fail_expected) {
+                fail_expected = false;
+                fail("Test passed when failure expected");
+            }
+
+            passed++;
+            printf("PASS\n");
+            break;
+        case RESULT_FAIL:
+            if (fail_expected) {
+                fail_expected = false;
+                printf("    (expected failure)\n    ");
+                pass();
+            }
+    
+            failed++;
+            /* Message already printed before the stack unwound */
+            break;
+        case RESULT_IGNORE:
+            ignored++;
+            printf("IGNORED\n");
+            break;
+        default:
+            printf("FATAL ERROR\n");
+            exit(1);
+            break;
     }
 }
 
