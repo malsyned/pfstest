cc899be40b7b42a6dc93a677121ee6b419791858
Dennis Lambe Jr <malsyned@malsyned.net>
2017-09-12 17:28:26 -0400

    mock-dep.c: Match identifiers, formatting to expected auto-mock output

--- mock-dep.c
+++ mock-dep.c
@@ -9,34 +9,38 @@ pfstest_mock_define(mock_dep_func1, "dep_func1", 1);
 
 int dep_func1(int i)
 {
-    int default_return = 0;
+    int __pfstest_default_return = 0;
 
-    pfstest_value_t *return_value =
-        pfstest_mock_invoke(mock_dep_func1, the_pointer(&default_return),
+    pfstest_value_t *__pfstest_return_value =
+        pfstest_mock_invoke(mock_dep_func1,
+                            the_pointer(&__pfstest_default_return),
                             the_pointer(&i));
 
-    return *(int *)pfstest_value_data(return_value);
+    return *(int *)pfstest_value_data(__pfstest_return_value);
 }
 
 pfstest_mock_define(mock_dep_func2, "dep_func2", 2);
 
 void dep_func2(int i, char *s)
 {
-    pfstest_value_t *return_value =
-        pfstest_mock_invoke(mock_dep_func2, NULL,
-                            the_pointer(&i), the_pointer(s));
+    pfstest_value_t *__pfstest_return_value =
+        pfstest_mock_invoke(mock_dep_func2,
+                            NULL,
+                            the_pointer(&i),
+                            the_pointer(s));
 
-    (void)return_value;
+    (void)__pfstest_return_value;
 }
 
 pfstest_mock_define(mock_dep_func3, "dep_func3", 0);
 
 char *dep_func3(void)
 {
-    char *default_return = NULL;
+    char * __pfstest_default_return = NULL;
 
-    pfstest_value_t *return_value =
-        pfstest_mock_invoke(mock_dep_func3, the_pointer(default_return));
+    pfstest_value_t *__pfstest_return_value =
+        pfstest_mock_invoke(mock_dep_func3,
+                            the_pointer(__pfstest_default_return));
 
-    return (char *)pfstest_value_data(return_value);
+    return (char *)pfstest_value_data(__pfstest_return_value);
 }
