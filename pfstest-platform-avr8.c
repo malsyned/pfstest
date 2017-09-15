341e644ba9588f1de84d99427ad46ceee9712193
Dennis Lambe Jr <malsyned@malsyned.net>
2017-09-15 17:46:15 -0400

    platform-avr8: Optional support for using __flash for nv functions

--- pfstest-platform-avr8.c
+++ pfstest-platform-avr8.c
@@ -1,5 +1,63 @@
 #include "pfstest-platform-avr8.h"
 
+#if defined(PFSTEST_AVR8_NAMED_ADDR_SPACES) && defined(__FLASH)
+
+void *pfstest_memcpy_nv(void *dest, const pfstest_nv_ptr void *src, size_t n)
+{
+    char *d = dest;
+    const pfstest_nv char *s = src;
+    size_t i;
+
+    for (i = 0; i < n; i++) {
+        d[i] = s[i];
+    }
+    return dest;
+}
+
+int pfstest_strcmp_nv(const char *s1, const pfstest_nv_ptr char *s2)
+{
+    for (; *s1 == *s2; s1++, s2++) {
+        if (*s1 == '\0')
+            return 0;
+    }
+    return *s1 - *s2;
+}
+
+int pfstest_strcmp_nvnv(const pfstest_nv_ptr char *s1,
+                        const pfstest_nv_ptr char *s2)
+{
+    for (; *s1 == *s2; s1++, s2++) {
+        if (*s1 == '\0')
+            return 0;
+    }
+    return *s1 - *s2;
+}
+
+char *pfstest_strcat_nv(char *dest, const pfstest_nv_ptr char *src)
+{
+    char *d = dest;
+    const pfstest_nv char *s = src;
+
+    while (*d != '\0')
+        d++;
+    while ((*d++ = *s++) != '\0')
+        ;
+
+    return dest;
+}
+
+const pfstest_nv_ptr char *pfstest_strchr_nv(const pfstest_nv_ptr char *s,
+                                             int c)
+{
+    while (*s != (char)c) {
+        if (!*s++)
+            return NULL;
+    }
+    return s;
+}
+
+#else /* !defined(__FLASH) */
+
 int pfstest_strcmp_nvnv(const char *s1, const char *s2)
 {
     char c1, c2;
@@ -9,3 +67,5 @@ int pfstest_strcmp_nvnv(const char *s1, const char *s2)
 
     return (unsigned char)c1 - (unsigned char)c2;
 }
+
+#endif /* !defined(__FLASH) */
--- pfstest-platform-avr8.h
+++ pfstest-platform-avr8.h
@@ -1,9 +1,27 @@
 #ifndef PFSTEST_PLATFORM_AVR8_H
 #define PFSTEST_PLATFORM_AVR8_H
 
-# include <stdbool.h>
-# include <stdint.h>
-# include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+#if defined(PFSTEST_AVR8_NAMED_ADDR_SPACES) && defined(__FLASH)
+
+# define pfstest_nv __flash
+# define pfstest_nv_ptr pfstest_nv
+/* pfstest_nv_string adapted from avr-libc PSTR macro */
+# define pfstest_nv_string(s) (__extension__({static const pfstest_nv char __c[] = (s); &__c[0];}))
+
+void *pfstest_memcpy_nv(void *dest, const pfstest_nv_ptr void *src, size_t n);
+int pfstest_strcmp_nv(const char *s1, const pfstest_nv_ptr char *s2);
+int pfstest_strcmp_nvnv(const pfstest_nv_ptr char *s1,
+                        const pfstest_nv_ptr char *s2);
+char *pfstest_strcat_nv(char *dest, const pfstest_nv_ptr char *src);
+const pfstest_nv_ptr char *pfstest_strchr_nv(const pfstest_nv_ptr char *s,
+                                             int c);
+
+#else /* !defined(__FLASH) */
+
 # include <avr/pgmspace.h>
 
 # define pfstest_nv PROGMEM
@@ -26,10 +44,12 @@
 # define pfstest_strcmp_nv(ram, nv) strcmp_P(ram, nv)
 # define pfstest_strcat_nv(ram, nv) strcat_P(ram, nv)
 # define pfstest_strchr_nv(nv, c) strchr_P(nv, c)
+int pfstest_strcmp_nvnv(const char *s1, const char *s2);
+
+#endif /* !defined(__FLASH) */
+
 # define PFSTEST_NORETURN __attribute__((__noreturn__))
 # define pfstest_constructor(name)                          \
     __attribute__((__constructor__)) static void name(void)
-
-int pfstest_strcmp_nvnv(const char *s1, const char *s2);
 
 #endif /* !PFSTEST_PLATFORM_AVR8_H */
