d9eb8d960d351ff89c64dd84695b7ce7d1ed13f7
Dennis Lambe Jr <malsyned@malsyned.net>
2017-09-23 16:28:03 -0400

    gcc-main.c: Removed malloc/free checking. Spurious failures on Ubuntu zesty

--- gcc-main.c
+++ gcc-main.c
@@ -1,15 +1,6 @@
-#include <malloc.h>
-#include <assert.h>
-
 #include "pfstest.h"
 
 int main(int argc, char *argv[])
 {
-    int r = pfstest_main(argc, argv);
-
-    struct mallinfo mi = mallinfo();
-
-    assert(mi.uordblks == 0);
-
-    return r;
+    return pfstest_main(argc, argv);
 }
