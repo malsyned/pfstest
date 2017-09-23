6e661f41b24b66b82fdf2220b07ded1b724a7384
Dennis Lambe Jr <malsyned@malsyned.net>
2017-09-23 16:51:27 -0400

    automock: Add some headers to aid manual integration testing

---
+++ automock/integration/test-header.h
@@ -0,0 +1,12 @@
+#ifndef TEST_HEADER_H
+#define TEST_HEADER_H
+
+#include "all-headers.h"
+
+void foo(void);
+int bar(void);
+unsigned int baz(void);
+char *quux(void);
+struct foost dennisx(void);
+
+#endif /* !TEST_HEADER_H */
