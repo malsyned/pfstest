6d4cd2c502d4ec8e5332935da26c7b35451bca97
Dennis Lambe Jr <malsyned@malsyned.net>
2017-09-30 13:39:14 -0400

    automock tests: Use ast.show instead of expand_decl to test AST equality

--- automock/tests/test_ExtensibleCParser.py
+++ automock/tests/test_ExtensibleCParser.py
@@ -1,12 +1,6 @@
 from unittest import TestCase
 
-# pycparser.c_ast.FileAST doesn't have a useful __eq__ method for use
-# with assertEqual. However, the pycparser test suite contains utility
-# functions for converting ast subtrees to nested lists for comparison
-import sys, os.path
-sys.path.append(os.path.join(os.path.dirname(__file__),
-                             '..','pycparser','tests'))
-from test_c_parser import expand_decl
+from compat import StringIO
 
 from extensiblecparser import ExtensibleCParser
 
@@ -22,6 +16,9 @@ cparser = ExtensibleCParser(storage_class_specifiers=['overlay'],
                             yacc_optimize = False)
 
 class ExtensibleCParserTests(TestCase):
+    def setUp(self):
+        self.showbuf = StringIO()
+
     def test_shouldAcceptNewStorageClassSpecifiers(self):
         pass
 
@@ -30,15 +27,17 @@ class ExtensibleCParserTests(TestCase):
 
     def test_shouldAcceptNewTypes(self):
         ast = cparser.parse('__builtin_va_list va; a_builtin_type abt;')
+        ast.show(buf=self.showbuf, nodenames=True, attrnames=True)
 
-        self.assertEqual(expand_decl(ast.ext[0]),
-                         ['Decl', 'va',
-                          ['TypeDecl',
-                           ['IdentifierType', ['__builtin_va_list']]]])
-        self.assertEqual(expand_decl(ast.ext[1]),
-                         ['Decl', 'abt',
-                          ['TypeDecl',
-                           ['IdentifierType', ['a_builtin_type']]]])
+        self.assertEqual(self.showbuf.getvalue(),
+                         """FileAST: 
+  Decl <ext[0]>: name=va, quals=[], storage=[], funcspec=[]
+    TypeDecl <type>: declname=va, quals=[]
+      IdentifierType <type>: names=['__builtin_va_list']
+  Decl <ext[1]>: name=abt, quals=[], storage=[], funcspec=[]
+    TypeDecl <type>: declname=abt, quals=[]
+      IdentifierType <type>: names=['a_builtin_type']
+""")
 
     def test_shouldAcceptNewFunctionSpecifiers(self):
         pass
