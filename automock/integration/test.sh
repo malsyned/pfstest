982b243dc48b57ecd36e8efc7534ebd23f74b89b
Dennis Lambe Jr <malsyned@malsyned.net>
2017-09-30 18:34:48 -0400

    Makefiles: Use automock extra keyword support for GCC compatibility

--- automock/integration/test.sh
+++ automock/integration/test.sh
@@ -5,6 +5,9 @@ set -e
 HEADERPATH=${1:-all-headers.h}
 MOCKROOT=${2:-mock-all-headers}
 
+-t __builtin_va_list -f __inline -f __inline__ -q __const -q __restrict
+
 gcc -E -D"__attribute__(x)=" -D"__asm__(x)=" -D"__extension__=" $HEADERPATH \
-    | ../automock.py -q __restrict -f __inline -t __builtin_va_list \
+    | ../automock.py -t __builtin_va_list -f __inline -f __inline__ \
+		     -q __const -q __restrict \
 		     $HEADERPATH $MOCKROOT
