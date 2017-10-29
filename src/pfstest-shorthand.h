4b731deb5d6df2da47b52d6558554304400932f8
Dennis Lambe Jr <malsyned@malsyned.net>
2017-10-29 02:07:05 -0400

    shorthand: Don't require boxing

--- src/pfstest-shorthand.h
+++ src/pfstest-shorthand.h
@@ -7,12 +7,12 @@
 #include "pfstest-values.h"
 #include "pfstest-matchers.h"
 
-#define pfstest_assert_true(message, value)                         \
-    pfstest_assert_that(message, value, is(the_bool(pfstest_true)))
+#define pfstest_assert_true(message, value)                             \
+    pfstest_assert_that(message, the_bool(value), is(the_bool(pfstest_true)))
 #define pfstest_assert_false(message, value)                            \
-    pfstest_assert_that(message, value, is(the_bool(pfstest_false)))
-#define pfstest_assert_null(message, value)                     \
-    pfstest_assert_that(message, value, is(the_pointer(NULL)))
+    pfstest_assert_that(message, the_bool(value), is(the_bool(pfstest_false)))
+#define pfstest_assert_null(message, value)                             \
+    pfstest_assert_that(message, the_pointer(value), is(the_pointer(NULL)))
 
 #ifndef PFSTEST_NOALIAS_assert_true
 # define assert_true pfstest_assert_true
--- tests/test-shorthand.c
+++ tests/test-shorthand.c
@@ -6,17 +6,15 @@
 
 test(should_assert_true)
 {
-    assert_true("assert_true passes on true input", the_bool(pfstest_true));
+    assert_true("assert_true passes on true input", pfstest_true);
 }
 
 test(should_assert_false)
 {
-    assert_false("assert_false passes on false input",
-                 the_bool(pfstest_false));
+    assert_false("assert_false passes on false input", pfstest_false);
 }
 
 test(should_assert_null)
 {
-    assert_null("assert_null passes on NULL pointers",
-                the_pointer(NULL));
+    assert_null("assert_null passes on NULL pointers", NULL);
 }
