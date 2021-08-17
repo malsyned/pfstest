2e4c6be6c2c6e1fa5faa2e27bf068cc89040a8d4
Dennis Lambe Jr <dennis@sparkcharge.io>
2021-08-17 17:08:16 -0400

    gcc-main.c: enable ANSI escape processing on stdout under mingw

--- src/main/gcc-main.c
+++ src/main/gcc-main.c
@@ -3,7 +3,26 @@
 #include "pfstest.h"
 #include "pfstest-cli.h"
 
+#if defined(_WIN32)
+# include <windows.h>
+#endif
+
+static void enable_ansi_color(void)
+{
+#if defined(_WIN32)
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (handle != INVALID_HANDLE_VALUE) {
+        DWORD mode = 0;
+        if (GetConsoleMode(handle, &mode)) {
+            mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            SetConsoleMode(handle, mode);
+        }
+    }
+#endif
+}
+
 int main(int argc, char *argv[])
 {
+    enable_ansi_color();
     return pfstest_main(argc, argv);
 }
